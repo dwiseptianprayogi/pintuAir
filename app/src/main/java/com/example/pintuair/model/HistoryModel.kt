67ae1cfde113f2473558eb8d43b6a0566d465494
DWI-PC <dwiseptian2498@gmail.com>
2024-05-30 22:35:33 +0700

    30/05/2024 22:34

---
+++ app/src/main/java/com/example/pintuair/model/HistoryModel.kt
@@ -0,0 +1,16 @@
+package com.example.pintuair.model
+
+class HistoryModel (StatPintu:String, TinggiAir:String, KelTanah:String, ArusAir:String) {
+
+    var StatPintu = ""
+    var TinggiAir = ""
+    var KelTanah = ""
+    var ArusAir = ""
+
+    init {
+        this.StatPintu = StatPintu
+        this.TinggiAir = TinggiAir
+        this.KelTanah = KelTanah
+        this.ArusAir = ArusAir
+    }
+}
